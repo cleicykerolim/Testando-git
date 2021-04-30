e61f18996c9f3a60df532779b27d2ba4edd00553
cleicykerolim <cleicykerolim@gmail.com>
2021-04-29 22:59:47 -0300

    teste com react

---
+++ reactnext/src/pages/index.tsx
@@ -0,0 +1,17 @@
+// SPA
+// SSR
+// SSG
+
+// import { useEffect } from "react"
+
+export default function Home() {
+	// useEffect(() => {
+	// 	fetch('http://localhost:3000/episodes')
+	// 	.then(response => response.json())
+	// 	.then(data => console.log(data))
+	// }, [])
+
+ 	return (
+ 		<h1>Index</h1>
+  	)
+}
